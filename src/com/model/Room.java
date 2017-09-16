afda25e85beb7565d33af57fa1a2b0588f64a365
SUDARAT\sp <sp.sudaratphiwon@hotmail.com>
2017-09-16 14:42:49 +0700

    Create method

--- src/com/model/Room.java
+++ src/com/model/Room.java
@@ -1,20 +1,30 @@
 package com.model;
 
+import javax.jws.WebParam;
+
+import org.bson.types.ObjectId;
+
+import com.connect.Connect;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+
 public class Room {
 	private String id;
-	private String roomID;
+	private int roomID;
 	private String type;
-	private String price;
+	private double price;
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getRoomID() {
+	public int getRoomID() {
 		return roomID;
 	}
-	public void setRoomID(String roomid) {
+	public void setRoomID(int roomid) {
 		this.roomID = roomid;
 	}
 	public String getType() {
@@ -23,10 +33,69 @@ public class Room {
 	public void setType(String type) {
 		this.type = type;
 	}
-	public String getPrice() {
+	public double getPrice() {
 		return price;
 	}
-	public void setPrice(String price) {
+	public void setPrice(double price) {
 		this.price = price;
 	}
+	public boolean create(int roomID,String type,double price) {
+		DB db = new Connect().mongo();
+		DBCollection collection = db.getCollection("room");
+		
+		
+		BasicDBObject document = new BasicDBObject();
+		document.put("roomID", roomID);
+		document.put("type", type);
+		document.put("price", price);
+
+		collection.insert(document);
+		
+		return true;
+	}
+	public boolean update(String id, int roomID,String type,double price ){
+		DB db = new Connect().mongo();
+		DBCollection collection = db.getCollection("course");
+		
+		BasicDBObject document = new BasicDBObject();
+		document.put("roomID", roomID);
+		document.put("type", type);
+		document.put("price", price);
+		
+		BasicDBObject setQuery = new BasicDBObject();
+        setQuery.put("$set", document);
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("_id", new ObjectId(id));
+
+		collection.update(searchQuery, setQuery);
+		
+		return true;  
+	}
+	public boolean delete(String id) {
+		DB db = new Connect().mongo();
+		DBCollection collection = db.getCollection("room");
+		
+		DBObject document = collection.findOne(new ObjectId(id));
+		collection.remove(document);
+		
+		return true;  
+	}
+	
+	
+	public Room getUpdate(@WebParam(name = "Room") String id) {
+		DB db = new Connect().mongo();
+		DBCollection table = db.getCollection("Room");
+		
+		DBObject object = table.findOne(new ObjectId(id));
+		
+		Room course = new Room();
+		course.setId(object.get("_id").toString());
+		course.setRoomID((int) object.get("roomID"));
+		course.setType(object.get("type").toString());
+		course.setPrice((double) object.get("price"));
+		
+		return course;  
+	}
+	
 }
